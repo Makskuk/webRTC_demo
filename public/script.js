de1a690372bde460302a89204e44467efd6cc2e4
Maksim Kuklin <makskuk@gmail.com>
2014-10-30 14:22:37 +0500

    add OFFER and ANSWER processing

--- public/script.js
+++ public/script.js
@@ -1,19 +1,15 @@
 var localStream = null;
-var pc1;
+var pc1, pc2;
 var servers = null;
 var offerConstraints = {};
 
 // Запрашиваем доступ и к аудио, и к видео
 var streamConstraints = { 
-		"audio": true
-		/*"video":{
-			"mandatory": { 
-				"maxWidth": "320",
-				"maxHeight": "240",
-				"maxFrameRate": "5" 
-			},
+		"audio": true,
+		"video":{
+			"mandatory": { "chromeMediaSource": 'screen'  },
 			"optional": []
-		} */
+		}
 	}; 
 
 function getUserMedia_success(stream) {
@@ -50,7 +46,7 @@ function pc1_createOffer_success(desc) {
   pc1.setLocalDescription(desc);  // Зададим RTCPeerConnection, сформированный Offer SDP методом setLocalDescription. 
   // Когда дальняя сторона пришлет свой Answer SDP, его нужно будет задать методом setRemoteDescription 
   // Пока вторая сторона не реализована, ничего не делаем
-  // pc2_receivedOffer(desc);
+  pc2_receivedOffer(desc);
 }
 
 function pc1_createOffer_error(error){
@@ -62,7 +58,7 @@ function pc1_onicecandidate(event){
   if (event.candidate) {
     console.log("pc1_onicecandidate():\n"+ event.candidate.candidate.replace("\r\n", ""), event.candidate);
     // Пока вторая сторона не реализована, ничего не делаем
-    // pc2.addIceCandidate(new RTCIceCandidate(event.candidate));
+    pc2.addIceCandidate(new RTCIceCandidate(event.candidate));
   }
 }
 
@@ -83,3 +79,55 @@ function createOffer_click() {
     offerConstraints
   );
 }  
+
+function pc2_createAnswer_success(desc) {  
+  pc2.setLocalDescription(desc);
+  console.log("pc2_createAnswer_success()", desc.sdp);
+  pc1.setRemoteDescription(desc);
+}
+
+function pc2_createAnswer_error(error) {
+  console.log('pc2_createAnswer_error():', error);
+  alert('pc2_createAnswer_error()');
+}
+
+var answerConstraints = { 
+  'mandatory': { 'OfferToReceiveAudio':true, 'OfferToReceiveVideo':true } 
+};
+
+function pc2_receivedOffer(desc) {
+  console.log("pc2_receiveOffer()", desc);
+  // Создаем объект RTCPeerConnection для второго участника аналогично первому
+  pc2 = new webkitRTCPeerConnection(servers);
+  pc2.onicecandidate = pc2_onicecandidate; // Задаем обработчик события при появлении ICE-кандидата
+  pc2.onaddstream = pc2_onaddstream; // При появлении потока подключим его к HTML <video>
+  pc2.addStream(localStream); // Передадим локальный медиапоток (в нашем примере у второго участника он тот же, что и у первого)
+  // Теперь, когда второй RTCPeerConnection готов, передадим ему полученный Offer SDP (первому мы передавали локальный поток)
+   pc2.setRemoteDescription( new RTCSessionDescription(desc) );
+  // Запросим у второго соединения формирование данных для сообщения Answer
+  pc2.createAnswer( 
+    pc2_createAnswer_success,
+    pc2_createAnswer_error,
+    answerConstraints
+  );
+}
+
+function pc2_onicecandidate(event) {
+  if (event.candidate) {
+    console.log("pc2_onicecandidate():", event.candidate.candidate);
+    pc1.addIceCandidate(new RTCIceCandidate(event.candidate));
+  }
+}
+
+function pc2_onaddstream(event) {
+  console.log("pc_onaddstream()");
+  remoteVideo2.src = URL.createObjectURL(event.stream);
+}
+
+function btnHangupClick() {
+  // Отключаем локальное видео от HTML-элементов <video>, останавливаем локальный медиапоток, устанавливаем = null
+  localVideo1.src = ""; localStream.stop(); localStream = null;
+  // Для каждого из участников отключаем видео от HTML-элементов <video>, закрываем соединение, устанавливаем указатель = null
+  remoteVideo1.src = ""; pc1.close(); pc1 = null;
+  remoteVideo2.src = ""; pc2.close(); pc2 = null; 
+}
